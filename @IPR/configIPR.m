ade1e65bffbb65046df77de7cf5ef7161e7ca3f1
Anatoli Ulmer <anatoli.ulmer@gmail.com>
2020-09-15 01:13:17 +0200

    added description

--- @IPR/configIPR.m
+++ @IPR/configIPR.m
@@ -44,7 +44,7 @@ function obj = configIPR(obj)
     %% plot parameter
     obj.nStepsUpdatePlot                = Inf;
     obj.int_cm                          = 'imorgen';            % colormap index for Fourier domain (dropdown menu)
-    obj.rec_cm                          = 'b2r';               % colormap for Real domain (dropdown menu)
+    obj.rec_cm                          = 'b2r';                % colormap for Real domain (dropdown menu)
     obj.clims_scatt                     = ([-1,3]);             % colormap limits for Fourier domain (log10 scale!)
     obj.reconrange                      = 3;                    % range index for color map scaling (dropdown menu)
     obj.intpart                         = 1;
