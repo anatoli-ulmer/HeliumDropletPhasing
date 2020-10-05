99a1d9a2875e485b6dabd6ee570a1efac61e7102
Anatoli Ulmer <anatoli.ulmer@gmail.com>
2020-10-05 15:31:42 +0200

    fixed paths.pnccd

--- main.m
+++ main.m
@@ -18,6 +18,7 @@ else
     paths.recon = fullfile(paths.storage, 'recon');
     paths.pnccd_dcg = fullfile(paths.storage, 'all_hits_corrected_dark_cm_gain');
     paths.pnccd_dcgb = fullfile(paths.storage, 'all_hits_corrected_dark_cm_gain_bg');
+    paths.pnccd = paths.pnccd_dcg;
     save(pathFile, 'paths')
 end
 %% switch off for performance reasons if necessary:
